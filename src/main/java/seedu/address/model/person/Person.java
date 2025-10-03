087376c7c8ac93cca0a2ecf336bed638115f3e1a
tevyntan <tevyntanyb@gmail.com>
2025-10-03 15:26:14 +0800

    Change for checkstyle

--- src/main/java/seedu/address/model/person/Person.java
+++ src/main/java/seedu/address/model/person/Person.java
@@ -126,7 +126,7 @@ public class Person {
     public void matchPerson(Person person) {
         if (this.isMatched || person.getMatchedStatus()) {
             return;
-        } else if (isTutor ^ person.isTutor() ) {
+        } else if (isTutor ^ person.isTutor()) {
             this.matchedPerson = person;
             person.setMatchedPerson(this);
         } else {

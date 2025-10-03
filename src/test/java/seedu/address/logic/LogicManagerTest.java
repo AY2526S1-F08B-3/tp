77e204e7447b5ef765487f786b5a48ff709cc242
Jun Wei <156562637+junwei07@users.noreply.github.com>
2025-10-04 07:40:15 +0800

    Fix LogicManagerTest issue

--- src/test/java/seedu/address/logic/LogicManagerTest.java
+++ src/test/java/seedu/address/logic/LogicManagerTest.java
@@ -66,10 +66,10 @@ public class LogicManagerTest {
 
     @Test
     public void execute_validCommand_success() throws Exception {
-        String listCommand = ListCommand.COMMAND_WORD;
-        assertCommandSuccess(listCommand, ListCommand.MESSAGE_SUCCESS, model);
+        String listCommand = ListCommand.COMMAND_WORD + " tutors";
+        assertCommandSuccess(listCommand, ListCommand.MESSAGE_SUCCESS_TUTORS, model);
     }
-
+    
     @Test
     public void execute_storageThrowsIoException_throwsCommandException() {
         assertCommandFailureForExceptionFromStorage(DUMMY_IO_EXCEPTION, String.format(
